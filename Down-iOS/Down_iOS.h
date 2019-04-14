628a48e7c65a20d2d707ca0a3c4e1984dbe9eafe
David House <david.house@kp.org>
2019-04-13 21:54:07 -0400

    :rocket: Added Down-iOS target

---
+++ Down-iOS/Down_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Down_iOS.h
+//  Down-iOS
+//
+//  Created by David House on 4/13/19.
+//  Copyright © 2019 Glazed Donut, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Down_iOS.
+FOUNDATION_EXPORT double Down_iOSVersionNumber;
+
+//! Project version string for Down_iOS.
+FOUNDATION_EXPORT const unsigned char Down_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Down_iOS/PublicHeader.h>
+
+
