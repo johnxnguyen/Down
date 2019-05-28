c3f54a64fafeab9b1430f4d0b6b5d19ab9bcc3dc
Aasim Kandrikar <aasimk@umich.edu>
2019-05-28 16:04:05 +0530

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Down",
+    platforms: [
+        .macOS("10.11"),
+        .iOS("9.0"),
+        .tvOS("9.0"),
+        .watchOS("2.0"),
+    ],
+    products: [
+    .library(
+        name: "Down",
+        targets: ["Down"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "libcmark",
+            dependencies: [],
+            path: "Source/cmark"),
+        .target(
+            name: "Down",
+            dependencies: ["libcmark"],
+            path: "Source/",
+            exclude: ["cmark", "Down.h"]),
+    ]
+)
