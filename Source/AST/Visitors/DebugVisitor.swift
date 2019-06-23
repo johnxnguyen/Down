d914382b63667449e4292a1c965e0ecf8e185644
John Nguyen <johnxuannguyen@gmail.com>
2019-06-23 14:25:13 +0200

    Allow DebugVisitor methods to be overridden

--- Source/AST/Visitors/DebugVisitor.swift
+++ Source/AST/Visitors/DebugVisitor.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// This visitor will generate the debug description of an entire abstract syntax tree,
 /// indicating relationships between nodes with indentation.
-public class DebugVisitor {
+public class DebugVisitor: Visitor {
     
     /// Current depth in the tree.
     private var depth = 0
@@ -34,88 +34,89 @@ public class DebugVisitor {
         depth -= 1
         return "\(thisNode)\(children)"
     }
-}
 
-extension DebugVisitor: Visitor {
+    // MARK: - Visitor
+
     public typealias Result = String
-    
+
     public func visit(document node: Document) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(blockQuote node: BlockQuote) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(list node: List) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(item node: Item) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(codeBlock node: CodeBlock) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(htmlBlock node: HtmlBlock) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(customBlock node: CustomBlock) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(paragraph node: Paragraph) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(heading node: Heading) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(thematicBreak node: ThematicBreak) -> String {
         return report(node)
     }
-    
+
     public func visit(text node: Text) -> String {
         return report(node)
     }
-    
+
     public func visit(softBreak node: SoftBreak) -> String {
         return report(node)
     }
-    
+
     public func visit(lineBreak node: LineBreak) -> String {
         return report(node)
     }
-    
+
     public func visit(code node: Code) -> String {
         return report(node)
     }
-    
+
     public func visit(htmlInline node: HtmlInline) -> String {
         return report(node)
     }
-    
+
     public func visit(customInline node: CustomInline) -> String {
         return report(node)
     }
-    
+
     public func visit(emphasis node: Emphasis) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(strong node: Strong) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(link node: Link) -> String {
         return reportWithChildren(node)
     }
-    
+
     public func visit(image node: Image) -> String {
         return reportWithChildren(node)
     }
 }
+
