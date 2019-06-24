67b89cf6f5516bae0647efb05beaa00862877abf
Matt Diephouse <matt@diephouse.com>
2019-06-24 19:55:56 -0400

    Fix permissions error on Catalina beta 2

--- Source/Views/DownView.swift
+++ Source/Views/DownView.swift
@@ -95,7 +95,7 @@ open class DownView: WKWebView {
     private lazy var temporaryDirectoryURL: URL = {
         return try! FileManager.default.url(for: .itemReplacementDirectory,
                                             in: .userDomainMask,
-                                            appropriateFor: URL(fileURLWithPath: "/"),
+                                            appropriateFor: URL(fileURLWithPath: NSTemporaryDirectory()),
                                             create: true).appendingPathComponent("Down", isDirectory: true)
     }()
     #endif
