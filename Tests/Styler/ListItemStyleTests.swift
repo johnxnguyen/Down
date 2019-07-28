5b3d6aaf93004b205424b24f5ee80c573ec99a27
John Nguyen <johnxuannguyen@gmail.com>
2019-07-28 20:37:22 +0200

    Add item prefix tests

--- Tests/Styler/ListItemStyleTests.swift
+++ Tests/Styler/ListItemStyleTests.swift
@@ -7,25 +7,81 @@
 //
 
 import XCTest
+import SnapshotTesting
 
 class ListItemStyleTests: StylerTestSuite {
 
     // MARK: - Prefix Alignment
 
     func testThat_DigitPrefixes_UpToMaxPrefixLength_Align() throws {
-        XCTFail()
+        // Given
+        let markdown = """
+        6. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        7. metus, non placerat velit cursus non. Integer suscipit erat placera
+        8. nisi faucibus, convallis enim non
+        9. consequat molestie leo
+        10. quis pulvinar libero placerat sit
+        11. ac rutrum nunc, eget rhoncus nunc
+        12. imperdiet pulvinar a eget urna
+        """
+
+        // When
+        let result = try view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
     }
 
     func testThat_DigitAndBulletPrefixes_Align() throws {
-        XCTFail()
+        // Given
+        let markdown = """
+        10. quis pulvinar libero placerat sit
+        11. ac rutrum nunc, eget rhoncus nunc
+        12. imperdiet pulvinar a eget urna
+
+        - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        - metus, non placerat velit cursus non. Integer suscipit erat placera
+        - nisi faucibus, convallis enim non
+        """
+
+        // When
+        let result = try view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
     }
 
     func testThat_DigitPrefixes_ExceedingMaxPrefixLength_Push_FirstLine() throws {
-        XCTFail()
+        // Given
+        let markdown = """
+        96. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        97. metus, non placerat velit cursus non. Integer suscipit erat placera
+        98. nisi faucibus, convallis enim non
+        99. consequat molestie leo
+        100. quis pulvinar libero placerat sit
+        101. ac rutrum nunc, eget rhoncus nunc
+        102. imperdiet pulvinar a eget urna
+        """
+
+        // When
+        let result = try view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
     }
 
     func testThat_DigitPrefixes_ExceedingMaxPrefixLength_DontPush_WrappedLines() throws {
-        XCTFail()
+        // Given
+        let markdown = """
+        99. Phasellus facilisis, nulla non tristique tempor, ligula nisi dignissim libero.
+        100. vitae sodales metus velit vitae est. Aliquam a dolor magna. In fermentum mattis urna
+        """
+
+        // When
+        let result = try view(for: markdown, width: .narrow)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
     }
 
     // MARK: - Paragraph Alignment
