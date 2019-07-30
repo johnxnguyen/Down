97e5a881bda6c9e9bcc7437991499f319b449856
John Nguyen <johnxuannguyen@gmail.com>
2019-07-30 22:05:14 +0200

    Add heading style tests

---
+++ Tests/Styler/HeadingStyleTests.swift
@@ -0,0 +1,124 @@
+//
+//  HeadingStyleTests.swift
+//  DownTests
+//
+//  Created by John Nguyen on 30.07.19.
+//  Copyright © 2019 Glazed Donut, LLC. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+
+class HeadingStyleTests: StylerTestSuite {
+
+    /// # Important
+    ///
+    /// Snapshot tests must be run on the same simulator used to record the reference snapshots, otherwise
+    /// the comparison may fail. These tests were recorded on the **iPhone Xs** simulator.
+    ///
+
+    // MARK: - Heading Levels
+
+    func testThat_Heading_LevelOne_IsStyled() {
+        // Given
+        let markdown = """
+        Praesent facilisis pellentesque ipsum at pulvinar. Sed consectetur augue vel mattis hendrerit.
+
+        # Quisque molestie auctor neque. Donec vitae risus non odio viverra hendrerit.
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+        """
+
+        // When
+        let result = view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image )
+    }
+
+    func testThat_Heading_LevelTwo_IsStyled() {
+        // Given
+        let markdown = """
+        Praesent facilisis pellentesque ipsum at pulvinar. Sed consectetur augue vel mattis hendrerit.
+
+        ## Quisque molestie auctor neque. Donec vitae risus non odio viverra hendrerit.
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+        """
+
+        // When
+        let result = view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
+    }
+
+    func testThat_Heading_LevelThree_IsStyled() {
+        // Given
+        let markdown = """
+        Praesent facilisis pellentesque ipsum at pulvinar. Sed consectetur augue vel mattis hendrerit.
+
+        ### Quisque molestie auctor neque. Donec vitae risus non odio viverra hendrerit.
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+        """
+
+        // When
+        let result = view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
+    }
+
+    func testThat_Heading_LevelsThreeToSix_AreStyledEqually() {
+        // Given
+        let markdown = """
+        Praesent facilisis pellentesque ipsum at pulvinar. Sed consectetur augue vel mattis hendrerit.
+
+        ### Quisque molestie auctor neque. Donec vitae risus non odio viverra hendrerit.
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+
+        Praesent facilisis pellentesque ipsum at pulvinar. Sed consectetur augue vel mattis hendrerit.
+
+        #### Quisque molestie auctor neque. Donec vitae risus non odio viverra hendrerit.
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+
+        Praesent facilisis pellentesque ipsum at pulvinar. Sed consectetur augue vel mattis hendrerit.
+
+        ##### Quisque molestie auctor neque. Donec vitae risus non odio viverra hendrerit.
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+
+        Praesent facilisis pellentesque ipsum at pulvinar. Sed consectetur augue vel mattis hendrerit.
+
+        ###### Quisque molestie auctor neque. Donec vitae risus non odio viverra hendrerit.
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+        """
+
+        // When
+        let result = view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
+    }
+
+    // MARK: - Inline Presevation
+
+    func testThat_HeadingStyle_Preserves_StrongEmphasisAndMonospaceTraits() {
+        // Given
+        let markdown = """
+        # Text **strong _emphasized `code` <html>_**
+
+        Pellentesque non vulputate felis. Curabitur aliquam, nisl vitae vulputate eleifend, metus sapien eleifend.
+        """
+
+        // When
+        let result = view(for: markdown, width: .wide)
+
+        // Then
+        assertSnapshot(matching: result, as: .image)
+    }
+}
