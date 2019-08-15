a1059f83689d76322d3ff319bed3e5cfa6ae95ed
John Nguyen <johnxuannguyen@gmail.com>
2019-08-15 10:47:09 +0200

    Remove dynamic fonts

--- Source/AST/Styling/Attribute Collections/FontCollection.swift
+++ Source/AST/Styling/Attribute Collections/FontCollection.swift
@@ -11,11 +11,6 @@
 import Foundation
 import UIKit
 
-
-// TODO: Documentation -> When using dynamic fonts, we should make sure the text view property
-// `adjustsFontForContentSizeCategory` is actually false. Instead override `traitCollectionDidChange()` to
-// manually reload the content.
-
 public struct FontCollection {
 
     public var heading1 = UIFont.boldSystemFont(ofSize: 28)
@@ -37,37 +32,5 @@ public struct FontCollection {
         UIFont.monospacedDigitSystemFont(ofSize: 17, weight: .regular)
     }()
 }
-
-public extension FontCollection {
-
-    static let dynamicFonts: FontCollection = {
-
-        var fonts = FontCollection()
-        fonts.heading1 = .preferredFont(forTextStyle: .title1)
-        fonts.heading2 = .preferredFont(forTextStyle: .title2)
-        fonts.heading3 = .preferredFont(forTextStyle: .title3)
-        fonts.body = .preferredFont(forTextStyle: .body)
-        fonts.listItemPrefix = .monospacedDigitSystemFont(ofSize: fonts.body.pointSize, weight: .regular)
-
-        // TODO: Clean this
-        if #available(iOS 12, *) {
-            fonts.code = .monospacedSystemFont(ofSize: fonts.body.pointSize, weight: .regular)
-        }
-        else if let menlo = UIFont(name: "menlo", size: fonts.body.pointSize) {
-            if #available(iOS 11, *) {
-                fonts.code = UIFontMetrics(forTextStyle: .body).scaledFont(for: menlo)
-            }
-            else {
-                fonts.code = menlo
-            }
-        }
-        else {
-            fonts.code = fonts.body
-        }
-
-        return fonts
-    }()
-}
-
 
 #endif
