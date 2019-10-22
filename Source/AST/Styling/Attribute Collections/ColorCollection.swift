b99cbca24f7691dc3c3b5a8b1971427482733e8e
John Nguyen <johnxuannguyen@gmail.com>
2019-10-22 16:17:15 +0200

    Make color & font collection initializers public (#184)

--- Source/AST/Styling/Attribute Collections/ColorCollection.swift
+++ Source/AST/Styling/Attribute Collections/ColorCollection.swift
@@ -37,17 +37,43 @@ public protocol ColorCollection {
 
 public struct StaticColorCollection: ColorCollection {
 
-    public var heading1 = DownColor.black
-    public var heading2 = DownColor.black
-    public var heading3 = DownColor.black
-    public var body = DownColor.black
-    public var code = DownColor.black
-    public var link = DownColor.systemBlue
-    public var quote = DownColor.darkGray
-    public var quoteStripe = DownColor.darkGray
-    public var thematicBreak = DownColor(white: 0.9, alpha: 1)
-    public var listItemPrefix = DownColor.lightGray
-    public var codeBlockBackground = DownColor(red: 0.96, green: 0.97, blue: 0.98, alpha: 1)
+    public var heading1: DownColor
+    public var heading2: DownColor
+    public var heading3: DownColor
+    public var body: DownColor
+    public var code: DownColor
+    public var link: DownColor
+    public var quote: DownColor
+    public var quoteStripe: DownColor
+    public var thematicBreak: DownColor
+    public var listItemPrefix: DownColor
+    public var codeBlockBackground: DownColor
+
+    public init(
+        heading1: DownColor = .black,
+        heading2: DownColor = .black,
+        heading3: DownColor = .black,
+        body: DownColor = .black,
+        code: DownColor = .black,
+        link: DownColor = .blue,
+        quote: DownColor = .darkGray,
+        quoteStripe: DownColor = .darkGray,
+        thematicBreak: DownColor = .init(white: 0.9, alpha: 1),
+        listItemPrefix: DownColor = .lightGray,
+        codeBlockBackground: DownColor = .init(red: 0.96, green: 0.97, blue: 0.98, alpha: 1)
+    ) {
+        self.heading1 = heading1
+        self.heading2 = heading2
+        self.heading3 = heading3
+        self.body = body
+        self.code = code
+        self.link = link
+        self.quote = quote
+        self.quoteStripe = quoteStripe
+        self.thematicBreak = thematicBreak
+        self.listItemPrefix = listItemPrefix
+        self.codeBlockBackground = codeBlockBackground
+    }
 }
 
 #endif
