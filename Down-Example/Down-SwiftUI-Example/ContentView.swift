a6470be383c3c83d405485cbcdc5e56d15794432
Mikhail <wowivan2011@yandex.ru>
2021-06-01 15:31:54 +0300

    Added target with SwiftUI lifecycle

---
+++ Down-Example/Down-SwiftUI-Example/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Down-SwiftUI-Example
+//
+//  Created by Mikhail Ivanov on 01.06.2021.
+//  Copyright © 2021 Down. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
